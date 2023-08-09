574a01499e55094a8ba41607043bfad478f0a506
Tiffany Adams <tadams03@mail.roosevelt.edu>
2023-08-09 18:24:34 -0500

    edit

--- BlazorApp1/Data/Models/DeveloperRosterDBContext.cs
+++ BlazorApp1/Data/Models/DeveloperRosterDBContext.cs
@@ -23,7 +23,7 @@ namespace BlazorApp1.Data.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-REMPRIR\\SQLEXPRESS;Database=DeveloperRosterDB; Trusted_Connection=True; TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=Database=DeveloperRosterDB; Trusted_Connection=True; TrustServerCertificate=True");
             }
         }
 
